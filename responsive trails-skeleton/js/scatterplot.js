1fbec31b140828c03d9c0c1032307fc70805403a
ryanwu12210024 <wrl021221@outlook.com>
2025-04-26 13:09:12 -0400

    Add

--- responsive trails-skeleton/js/scatterplot.js
+++ responsive trails-skeleton/js/scatterplot.js
@@ -8,11 +8,10 @@ class Scatterplot {
     constructor(_config, _data) {
       this.config = {
         parentElement: _config.parentElement,
-        colorScale: _config.colorScale,
-        containerWidth: _config.containerWidth || 500,
-        containerHeight: _config.containerHeight || 300,
-        margin: _config.margin || {top: 25, right: 20, bottom: 20, left: 35},
-        tooltipPadding: _config.tooltipPadding || 15
+        containerHeight: 400, //no containerWidth specified
+
+        margin: {top: 25, right: 20, bottom: 20, left: 35},
+        tooltipPadding: 15
       }
       this.data = _data;
       this.initVis();
@@ -24,40 +23,26 @@ class Scatterplot {
     initVis() {
       let vis = this;
   
-      // Calculate inner chart size. Margin specifies the space around the actual chart.
-      vis.width = vis.config.containerWidth - vis.config.margin.left - vis.config.margin.right;
-      vis.height = vis.config.containerHeight - vis.config.margin.top - vis.config.margin.bottom;
+      // Initialize scales
+      vis.colorScale = d3.scaleOrdinal()
+          .range(['#d3eecd', '#7bc77e', '#2a8d46']) // light green to dark green
+          .domain(['Easy','Intermediate','Difficult']);
   
-      vis.xScale = d3.scaleLinear()
-          .range([0, vis.width]);
-  
-      vis.yScale = d3.scaleLinear()
-          .range([vis.height, 0]);
-
-      vis.symbolScale = d3.scaleOrdinal()
-          .range([
-            d3.symbol().type(d3.symbolCircle)(),
-            d3.symbol().type(d3.symbolSquare)(),
-            d3.symbol().type(d3.symbolDiamond)()
-          ])
-          .domain(['Easy', 'Intermediate', 'Difficult']);
+      vis.xScale = d3.scaleLinear();
+      vis.yScale = d3.scaleLinear();
   
       // Initialize axes
       vis.xAxis = d3.axisBottom(vis.xScale)
           .ticks(6)
-          .tickSize(-vis.height - 10)
           .tickPadding(10)
           .tickFormat(d => d + ' km');
   
       vis.yAxis = d3.axisLeft(vis.yScale)
           .ticks(6)
-          .tickSize(-vis.width - 10)
           .tickPadding(10);
   
       // Define size of SVG drawing area
-      vis.svg = d3.select(vis.config.parentElement)
-          .attr('width', vis.config.containerWidth)
-          .attr('height', vis.config.containerHeight);
+      vis.svg = d3.select(vis.config.parentElement).append('svg');
   
       // Append group element that will contain our actual chart 
       // and position it according to the given margin config
@@ -66,23 +51,20 @@ class Scatterplot {
   
       // Append empty x-axis group and move it to the bottom of the chart
       vis.xAxisG = vis.chart.append('g')
-          .attr('class', 'axis x-axis')
-          .attr('transform', `translate(0,${vis.height})`);
+          .attr('class', 'axis x-axis');
       
       // Append y-axis group
       vis.yAxisG = vis.chart.append('g')
           .attr('class', 'axis y-axis');
   
       // Append both axis titles
-      vis.chart.append('text')
+      vis.xAxisTitle = vis.chart.append('text')
           .attr('class', 'axis-title')
-          .attr('y', vis.height - 15)
-          .attr('x', vis.width + 10)
           .attr('dy', '.71em')
           .style('text-anchor', 'end')
           .text('Distance');
   
-      vis.svg.append('text')
+      vis.yAxisTitle = vis.svg.append('text')
           .attr('class', 'axis-title')
           .attr('x', 0)
           .attr('y', 0)
@@ -91,19 +73,51 @@ class Scatterplot {
     }
   
     /**
-     * Prepare the data and scales before we render it.
+     * Set the size of the SVG container, and prepare the data and scales before we render it.
      */
     updateVis() {
       let vis = this;
+  
+      // Update all dimensions based on the current screen size
+      vis.config.containerWidth = document
+          .getElementById(vis.config.parentElement.substring(1))
+          .clientWidth;
+      console.log("The current container width is:", vis.config.containerWidth);
+
+      // Calculate inner chart size. Margin specifies the space around the actual chart.
+      vis.config.width = vis.config.containerWidth - vis.config.margin.left - vis.config.margin.right;
+      vis.config.height = vis.config.containerHeight - vis.config.margin.top - vis.config.margin.bottom;
+
+      // Update SVG Size
+      vis.svg.attr('width', vis.config.containerWidth)
+	        .attr('height', vis.config.containerHeight);
+
+      vis.xAxisG
+          .attr('transform', `translate(0,${vis.config.height})`);
+  
+      vis.xAxisTitle
+          .attr('y', vis.config.height - 15)
+          .attr('x', vis.config.width + 10);
+  
+      vis.xAxis
+          .tickSize(-vis.config.height - 10);
+  
+      vis.yAxis
+          .tickSize(-vis.config.width - 10);
       
       // Specificy accessor functions
       vis.colorValue = d => d.difficulty;
-      vis.xValue = d => d.distance;
-      vis.yValue = d => d.time;
+      vis.xValue = d => d.time;
+      vis.yValue = d => d.distance;
   
       // Set the scale input domains
-      vis.xScale.domain([0, d3.max(vis.data, vis.xValue)]);
-      vis.yScale.domain([0, d3.max(vis.data, vis.yValue)]);
+      vis.xScale
+          .range([0, vis.config.width])
+          .domain([0, d3.max(vis.data, vis.xValue)]);
+      
+      vis.yScale
+          .range([vis.config.height, 0])
+          .domain([0, d3.max(vis.data, vis.yValue)]);
   
       vis.renderVis();
     }
@@ -113,33 +127,19 @@ class Scatterplot {
      */
     renderVis() {
       let vis = this;
-      /**
-     * Change the position slightly to better see if multiple symbols 
-     * share the same coordinates
-     */
-      function jitter(value) {
-        // this will get a number between 1 and 5;
-        var num = Math.floor(Math.random()*0.35) + 1; 
-
-        // this will add minus sign in 50% of cases
-        num *= Math.round(Math.random()) ? 1 : -1; 
-        console.log(num);
-        return value + num;
-      }
   
-      // Add symbols
-      const symbols = vis.chart.selectAll('.symbol')
+      // Add circles
+      const circles = vis.chart.selectAll('.point')
           .data(vis.data, d => d.trail)
-        .join('path')
-          .attr('class', 'symbol')
-          .attr('transform', d => 
-              `translate(${vis.xScale(jitter(vis.xValue(d)))}, 
-              ${vis.yScale(vis.yValue(d))})`)
-          .attr('d', d => vis.symbolScale(d.difficulty))
-          .attr('fill', d => vis.config.colorScale(vis.colorValue(d)));
+        .join('circle')
+          .attr('class', 'point')
+          .attr('r', 4)
+          .attr('cy', d => vis.yScale(vis.yValue(d)))
+          .attr('cx', d => vis.xScale(vis.xValue(d)))
+          .attr('fill', d => vis.colorScale(vis.colorValue(d)));
   
       // Tooltip event listeners
-      symbols
+      circles
           .on('mouseover', (event,d) => {
             d3.select('#tooltip')
               .style('display', 'block')
